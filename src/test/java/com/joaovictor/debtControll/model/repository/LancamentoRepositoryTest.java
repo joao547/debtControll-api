c6b5c66f38489d3b88ed20c9c8020efd5edfa3de
joao.victor01 <joao.victor@usto.re>
2020-06-27 13:59:28 -0300

    Criando teste de Integração do repository

---
+++ src/test/java/com/joaovictor/debtControll/model/repository/LancamentoRepositoryTest.java
@@ -0,0 +1,95 @@
+package com.joaovictor.debtControll.model.repository;
+
+import com.joaovictor.debtControll.model.entity.Lancamento;
+import com.joaovictor.debtControll.model.enums.StatusLancamento;
+import com.joaovictor.debtControll.model.enums.TipoLancamento;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+class LancamentoRepositoryTest {
+
+    @Autowired
+    LancamentoRepository repository;
+
+    @Autowired
+    TestEntityManager entityManager;
+
+    @Test
+    public void deveSalvarUmLancamento(){
+        Lancamento lancamento = criarLancamento();
+
+        lancamento = repository.save(lancamento);
+
+        assertNotNull(lancamento.getId());
+    }
+
+    @Test
+    public void deveDeletarUmLancamento(){
+        Lancamento lancamento = criarEPersistirLancamento();
+
+        lancamento = entityManager.find(Lancamento.class, lancamento.getId());
+
+        repository.delete(lancamento);
+
+        Lancamento lancamentoInexistente = entityManager.find(Lancamento.class, lancamento.getId());
+        assertNull(lancamentoInexistente);
+    }
+
+    @Test
+    public void deveAtualizarUmLancamento(){
+        Lancamento lancamento = criarEPersistirLancamento();
+
+        lancamento.setAno(2021);
+        lancamento.setDescricao("Nova descrição");
+        lancamento.setStatus(StatusLancamento.CANCELADO);
+        repository.save(lancamento);
+
+        Lancamento lancamentoAtualizado = entityManager.find(Lancamento.class, lancamento.getId());
+
+        assertEquals(lancamentoAtualizado.getAno(),2021);
+        assertEquals(lancamentoAtualizado.getDescricao(),"Nova descrição");
+        assertEquals(lancamentoAtualizado.getStatus(), StatusLancamento.CANCELADO);
+    }
+
+    @Test
+    public void deveBuscarUmLancamentoPorId(){
+        Lancamento lancamento = criarEPersistirLancamento();
+
+        Optional<Lancamento> lancamentoEncontrado = repository.findById(lancamento.getId());
+
+        assertTrue(lancamentoEncontrado.isPresent());
+    }
+
+    private Lancamento criarEPersistirLancamento() {
+        Lancamento lancamento = criarLancamento();
+        entityManager.persist(lancamento);
+        return lancamento;
+    }
+
+    private Lancamento criarLancamento(){
+        return Lancamento.builder()
+                .ano(2020)
+                .mes(6)
+                .descricao("teste")
+                .valor(BigDecimal.valueOf(100))
+                .tipo(TipoLancamento.RECEITA)
+                .status(StatusLancamento.PENDENTE)
+                .dataCadastro(LocalDate.now()).build();
+    }
+
+}
